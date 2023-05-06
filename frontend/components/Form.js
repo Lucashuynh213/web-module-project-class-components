1eb44f6156bc4afc599f77ce661959c11e831bd9
Lucas Huynh <nguyenhuynh21394@gmail.com>
2023-05-05 22:29:41 -0400

    completed Form

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,44 @@
 import React from 'react'
 
+
 export default class Form extends React.Component {
   render() {
     return (
-      <div>
-        Form
+      <div className="form-check">
+        <form>
+          <div className='checkradio'>
+          <input type='radio' value="" id="jogging"/>
+          <label for="jogging">
+            Jogging
+          </label><br></br>
+          <input type='radio' value="" id="walk"/>
+          <label for="walk">
+            Walk the dogs
+          </label><br></br>
+          <input type='radio' value="" id="play"/>
+          <label for="play">
+            Play Dota 2
+          </label><br></br>
+          <input type='radio' value="" id="concert"/>
+          <label for="concert">
+            Go see the concert
+          </label><br></br>
+          </div>
+          <div className='typing'>
+               
+            <input
+              label='Readonly'
+              placeholder='Readonly input here...'
+              id='formControlReadOnly'
+              type='text'
+              readonly
+            />
+            <button>Add</button>
+            <br></br>
+
+            <button className='clear'>Clear</button> 
+          </div>
+        </form>
       </div>
     )
   }
--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
+import Form from './components/Form'
 import './styles/reset.css'
 import './styles/styles.css'
 
@@ -8,6 +9,7 @@ render(
   <React.StrictMode>
     <h1>Todo App</h1>
     <App />
+    <Form/>
   </React.StrictMode>
   , document.getElementById('root')
 )
